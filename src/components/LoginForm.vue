aff2d2064144eb89e8c4f766443f8787066ad20d
Baptiste Angot <bat76640@gmail.com>
2023-07-19 14:35:58 +0200

    Authentification system

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -72,17 +72,16 @@ export default {
     },
     methods: {
         login() {
-            AuthService.login(this.username, this.password)
-                .then(() => {
-                    if (AuthProvider.isAuthenticated()) {
-                        this.$router.push("/");
-                    } else {
-                        this.errorMessage = "Identifiants invalides";
-                    }
-                })
-                .catch((error) => {
-                    this.errorMessage = error.message;
-                });
+            AuthService.login(this.username, this.password).then((r) => {
+                console.log(r)
+                if (AuthProvider.isAuthenticated()) {
+                    this.$router.push({ name: "Games"});
+                } else {
+                    this.errorMessage = "Identifiants invalides";
+                }
+            }).catch((e) => {
+                this.errorMessage = e;
+            });
         },
     },
 };
